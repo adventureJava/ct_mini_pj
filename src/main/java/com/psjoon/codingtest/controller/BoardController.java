733fb2cc0c6748c3cae0f72df852dec72aa23ea0
adventureJava <poker6711@naver.com>
2024-10-10 18:17:44 +0900

    Changes 레퍼지토리

--- src/main/java/com/psjoon/codingtest/controller/BoardController.java
+++ src/main/java/com/psjoon/codingtest/controller/BoardController.java
@@ -1,14 +1,17 @@
 package com.psjoon.codingtest.controller;
 
+import com.psjoon.codingtest.entity.Member;
 import com.psjoon.codingtest.entity.TestBoard;
 import com.psjoon.codingtest.service.TestBoardService;
 import com.psjoon.codingtest.util.PagingUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 
@@ -21,6 +24,10 @@ public class BoardController {
     @GetMapping("/test_board")
     public String goTestBoard(@RequestParam(defaultValue = "1") int pageNum, Model model) {
         int listCnt = 5; // 한 페이지에 표시할 게시글 수
+
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Member memberId = (Member) authentication.getPrincipal();
+            System.out.println("ID는 "+memberId.getId());
 
         // 페이징 처리된 게시글 목록 가져오기
         List<TestBoard> testBoardList = testBoardService.getTestBoards(pageNum, listCnt);
--- src/main/java/com/psjoon/codingtest/controller/MemberController.java
+++ src/main/java/com/psjoon/codingtest/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -42,9 +43,9 @@ public class MemberController {
 
         if (loginMember != null) {
             // 로그인 성공
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            String userId = authentication.getName();
-            System.out.println(userId);
+//            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//            Member memberId = (Member) authentication.getPrincipal();
+//            System.out.println("ID는 "+memberId.getUsername());
             return new ResponseEntity<>("로그인 성공", HttpStatus.OK);
         } else {
             // 로그인 실패
